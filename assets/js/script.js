58153bc53f485e4dd79ee5a29037133214150969
Kendal Asprec <kenasprec@berkeley.edu>
2017-11-26 19:36:49 -0800

    Finished index

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+
+    // jQuery Function Number 1
+    $('.button').hover(function () {
+        // jQuery Function Number 2
+        $(this).css('background', '#4D89FF');
+        $(this).css('transition', '0.4s');
+    }, function () {
+        $(this).css('background', '')
+        $(this).css('transition', '0.4s');
+    });
+
+    // jQuery Function Number 3
+    $('.button').click(function () {
+        $(this).css('background', '#FF0000');
+    });
+
+    $('#typeit').typeIt({
+        strings: ['Developer', 'Designer', 'Leader', 'Kendal Asprec'],
+        speed: 100,
+        breakLines: false
+    });
+
+});
