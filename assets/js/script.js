5fc6215be066e18b1e4820532971138c49ee65d6
Kendal Asprec <kenasprec@berkeley.edu>
2017-12-16 16:32:42 -0800

    Under massive renovations

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,10 +22,12 @@ $(document).ready(function () {
         });
     });
 
+    /*
     $('#typeit').typeIt({
         strings: ['Developer', 'Designer', 'Leader', 'Kendal Asprec'],
         speed: 100,
         breakLines: false
     });
+    */
 
 });
