49712bec76e1bfb70cf6a82a9f9a326624f36247
Kendal Asprec <kenasprec@berkeley.edu>
2017-12-20 18:58:05 -0800

    Added temporary landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,5 @@
 $(document).ready(function () {
 
-    // jQuery Function Number 1
-    $('.button').hover(function () {
-        // jQuery Function Number 2
-        $(this).css('background', '#4D89FF');
-        $(this).css('transition', '0.4s');
-    }, function () {
-        $(this).css('background', '')
-        $(this).css('transition', '0.4s');
-    });
-
-    // jQuery Function Number 3
-    $('.button').click(function () {
-        $(this).css('background', 'red');
-    });
-
-    $('#incomplete').click(function () {
-        // jQuery Function Number 4
-        $(this).fadeOut(function () {
-            $(this).text("Sorry!").fadeIn();
-        });
-    });
-
     /*
     $('#typeit').typeIt({
         strings: ['Developer', 'Designer', 'Leader', 'Kendal Asprec'],
